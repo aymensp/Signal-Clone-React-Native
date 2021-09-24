023800d464cc86b023737e0e5bcb08fee6f40cc4
aymensp <aymen.smati@esprit.tn>
2021-09-25 00:13:03 +0100

    Working on Login screen

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './screens/Login';
 
+
+const Stack = createNativeStackNavigator();
+const gloabalScreenOptions = {
+  headerStyle : {backgroundColor : "#2C6BED"} , 
+  headerTitleStyle : {color : 'white'} ,
+  headerTintColor : "white",
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={gloabalScreenOptions}>
+    
+      <Stack.Screen name='Login' component={ Login}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ3cH5CyP8hFhFtTEWEKxaPtBbIojfl8Y",
+    authDomain: "signal-clone-64922.firebaseapp.com",
+    projectId: "signal-clone-64922",
+    storageBucket: "signal-clone-64922.appspot.com",
+    messagingSenderId: "353515790050",
+    appId: "1:353515790050:web:6134a93368671fdf3248bb",
+    measurementId: "G-MTRJBN44RQ"
+  };
+  
